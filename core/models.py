f6db03407375f8beb6efe5e0990a1cdecdf5d1fd
Pashs-ba <pashs.ba@gmail.com>
2022-01-28 19:36:45 +0300

    models, again

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(verbose_name="Название", max_length=2000)
+    description = models.TextField(verbose_name="Описание")
+    big_image = models.ImageField(verbose_name="Подложка под название")
+    carusel_list = models.JSONField(null=True)
+
+
+class Item(models.Model):
+    name = models.CharField(verbose_name="Название", max_length=2000)
+    description = models.TextField(verbose_name="Описание")
+    image = models.ImageField(verbose_name="Изображение")
