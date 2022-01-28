35dd25d36966d3a65a6e92ebe725e8569f664598
Pashs-ba <pashs.ba@gmail.com>
2022-01-28 19:23:17 +0300

    fucking git

--- core/models.py
+++ core/models.py
@@ -1,15 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Category(models.Model):
-    name = models.CharField(verbose_name="Имя", max_length=2000)
-    big_pic = models.ImageField(verbose_name="Подложка под название")
-    description = models.TextField(verbose_name="Описание")
-
-    def __str__(self):
-        return self.name
-
-class Item(models.Model):
-    name = models.CharField(_verbose_name="Название товара", max_length=500)
-    description = models.TextField(verbose_name="Описание")
-    pic = models.ImageField(verbose_name="Изображение")
